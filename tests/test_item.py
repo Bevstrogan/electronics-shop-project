b4c4cd17e8f2b81832a25a728fc93bbf2d9681ba
Дмитрий Алехин <bevstrogan@gmail.com>
2023-12-09 17:03:21 +0300

    Выполнено дз 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+product = Item('Часы Xiaomi', 4500, 18)
+Item.pay_rate = 0.5
+def test_price():
+    assert Item.calculate_total_price(product) == 81000
+
+def test_discount():
+    assert Item.apply_discount(product) == 2250
